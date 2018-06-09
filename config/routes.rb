1767694890ed43b40c16736f9e8973301bf90ab4
Leonel Oliveros <leoneloliveros.co@gmail.com>
2018-06-08 22:47:08 -0500

    l

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Api definition
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :products, only: [:index, :create, :update, :delete]
+      resources :products, only: [:index, :create, :update, :destroy]
     end
   end
   # namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/'  do
